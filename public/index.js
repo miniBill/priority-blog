65dd35afd40c10320205ec06bb01aa072a50ea23
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-04-25 15:44:52 +0200

    Remove noise from index.js

--- public/index.js
+++ public/index.js
@@ -4,9 +4,8 @@
 export default {
   load: async function (elmLoaded) {
     const app = await elmLoaded;
-    console.log("App loaded", app);
   },
   flags: function () {
-    return "You can decode this in Shared.elm using Json.Decode.string!";
+    return "";
   },
 };
